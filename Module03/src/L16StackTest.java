f26d92498998b5ce1352dcfedc6d549704665d69
HaiDangNguyenLe <haidangln@gmail.com>
2025-03-28 20:50:54 -0600

    update module 3

---
+++ Module03/src/L16StackTest.java
@@ -0,0 +1,115 @@
+/*
+ * Stack Example
+ * Add comments to explain the code
+ */
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class L16StackTest {
+	public static void main(String[] args) {
+
+		//string stack
+		Stack<String> stringStack = new Stack<>();
+		
+		System.out.println("Pushing elements: 10, 20, 30");
+		stringStack.push("A");
+		stringStack.push("B");
+		stringStack.push("C");
+		System.out.println("stringStack after pushing: " + stringStack);
+		      
+		String stringPopped = stringStack.pop();
+		System.out.println("string Popped element: " + stringPopped);
+		System.out.println("stringStack after popping: " + stringStack);
+
+		//integer stack
+		Stack<Integer> stack = new Stack<>();
+		
+		System.out.println("Pushing elements: 10, 20, 30");
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		System.out.println("Stack after pushing: " + stack);  
+
+
+
+		int popped = stack.pop();
+		System.out.println("Popped element: " + popped);
+		System.out.println("Stack after popping: " + stack);
+
+
+		int topElement = stack.peek();
+		System.out.println("Top element: " + topElement);
+
+
+		System.out.println("Is stack empty? " + stack.isEmpty());
+
+		popped = stack.pop();
+		System.out.println("Popped element: " + popped);
+		System.out.println("Stack after popping: " + stack);
+
+		popped = stack.pop();
+		System.out.println("Popped element: " + popped);
+		System.out.println("Stack after popping: " + stack);
+
+		popped = stack.pop();
+		System.out.println("Popped element: " + popped);
+		System.out.println("Stack after popping: " + stack);
+	}
+}
+
+
+
+class Stack<T> {
+
+
+	private ArrayList<T> items; 
+
+
+	public Stack() {
+		this.items = new ArrayList<>();
+
+	}
+
+
+	public boolean isEmpty() {
+		return this.items.isEmpty();
+	}
+
+
+	public void push(T item) {
+		this.items.add(item);
+	}
+
+
+	public T pop() {
+		if (this.isEmpty()) {
+			throw new NoSuchElementException("Stack is empty.");
+		}
+		return this.items.remove(items.size() - 1);
+	}
+
+
+	public T peek() {
+		if (this.isEmpty()) {
+			throw new NoSuchElementException("Stack is empty.");
+		}
+		return this.items.get(items.size() - 1);
+	}
+
+
+	public int size() {
+		return this.items.size();
+	}
+
+
+	@Override
+	public String toString() {
+		if (!this.items.isEmpty()) {
+			return "bottom ->" + this.items.toString() + "<- top";
+		} else {
+			return "<<empty stack>>";
+		}
+	}
+}
+
