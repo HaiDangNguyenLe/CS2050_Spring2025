1b5fc923c2c0b7deeb4986c7a6b3f8402125ed21
HaiDangNguyenLe <haidangln@gmail.com>
2025-03-28 20:49:24 -0600

    update GE01 2DArray

--- GuideExplorationCs2050/src/HaiDangLeGE012DArray.java
+++ GuideExplorationCs2050/src/HaiDangLeGE012DArray.java
@@ -1,3 +1,15 @@
+/*
+ Name: HAIDANG LE
+ Class: CS2050 CS 2 With Java
+ Metropolitan State University of Denver
+ Spring 2025 
+ Instructor: Deb Harding (she/her/hers)    Email: dhardi11@msudenver.edu  Office: EAS 200BB
+ 
+ Description: Guide Exploration 01 - 2D array: explore a 2-dimensional array of objects
+	● Declare a 2D array of objects 
+	● Fill the array with car objects 
+	● Print array
+ */
 import java.util.Scanner;
 
 public class HaiDangLeGE012DArray {
@@ -7,14 +19,22 @@ public class HaiDangLeGE012DArray {
 		Scanner keyboard = new Scanner(System.in);
 		
 		//Declare a 2D array of objects 
-		int ROW = 2;
-		int COL = 3;
+		final int ROW = 2;
+		final int COL = 3;
 		Car cars[][] = new Car [ROW][COL];
 		//Fill the array with car objects 
-		for (int row = 0; row < cars.length; row++) {
+		cars[0][0]= new Car("Ford");
+		cars[0][1]= new Car("Dodge");
+		cars[0][2]= new Car("Toyota");
+		cars[1][0]= new Car("Huynhdai");
+		cars[1][1]= new Car("Chevrolet");
+		cars[1][2]= new Car("Subaru");
+		
+		/*for (int row = 0; row < cars.length; row++) {
 			for(int col = 0; col < cars[row].length; col++) {
 				//enter model for car object
-				System.out.print("Enter car make of row " + (row + 1) + " colume " + (col +1) +" : ");
+				System.out.print("Enter car model of row " + (row + 1) 
+									+ " colume " + (col +1) +" : ");
 				String model = keyboard.nextLine();
 				//construct car object with specific model
 				Car aCar = new Car(model);
@@ -22,7 +42,8 @@ public class HaiDangLeGE012DArray {
 				cars[row][col] = aCar;
 			
 			}
-		}
+		}*/
+				
 		//print array
 		for (int row = 0; row < cars.length; row++) {
 			for(int col = 0; col < cars[row].length; col++) {
@@ -34,7 +55,7 @@ public class HaiDangLeGE012DArray {
 		keyboard.close();
 	}//end main
 		
-}//end HaiDangLeGE022DArray
+}//end HaiDangLeGE012DArray
 class Car{
 	private String make;
 
@@ -49,5 +70,5 @@ public Car(String make) {
 public void printMake() {
    System.out.print(this.make + " ");
 }
-} // Car
+} // end Car
 
