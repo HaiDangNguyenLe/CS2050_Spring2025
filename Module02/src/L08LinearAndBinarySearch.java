b6e82013ee9bd786b36e531c4f9ba4bc7b495d38
HaiDangNguyenLe <haidangln@gmail.com>
2025-03-28 20:49:41 -0600

    update

--- Module02/src/L08LinearAndBinarySearch.java
+++ Module02/src/L08LinearAndBinarySearch.java
@@ -1,11 +1,56 @@
+import java.util.Arrays;
 
 public class L08LinearAndBinarySearch {
 
 	public static void main(String[] args) {
-		
+		int []list1 = {9,2,5,2,5,7};
+		int []list2 = {1,2,3,3,4,4,6,7,8};
+		//System.out.println("Linear search result:" +linearSearch(list1,2));
+		System.out.println("Binary search result: "+binarySearch(list2,4));
+
 	}//end main
-	
-	public static void linearSearch (){
-	
-	}
+
+	//linear search
+	public static int linearSearch (int [] list, int key){
+		//compare each element of array to key
+		//if the same then return the position
+		//if no element the same, return -1
+		for( int i=0; i< list.length; i++) {
+			if(key == list[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}//end linearSearch
+
+	// Binary search
+	//list must be sorted arrays
+	public static int binarySearch (int [] list, int key)	{
+		int low = 0;
+		int high = list.length - 1;
+		int index = -1; //If cannot found key, return -1
+		while (high > low ) {
+			int mid = (low + high)/2; //find mid
+			//update high if list[mid] > key
+			if (key < list[mid]) {
+				high = mid-1;
+			}
+			//return position = mid if key = list[mid]
+			else if (key == list[mid]) {
+				//update index if key is found
+				index = mid;
+				//continue search more on left side to find first occurrence 
+				high = mid-1;
+			} 
+			//update low if list[mid] < key
+			else {
+				low = mid +1;
+			}
+
+		}//end while
+		return index; //no thing found
+	}//end binary search
+
+
+
 }
--- Module02/src/SelectionBubleInsertSortTDD.java
+++ Module02/src/SelectionBubleInsertSortTDD.java
@@ -12,29 +12,30 @@ public class SelectionBubleInsertSortTDD
 				{ 9, 7, 5, 3, 1 }, // Reverse sorted
 				{ 4, 2, 7, 2, 5 } // Array with duplicates
 		};
-		//selection sort
+		/*//selection sort
 		System.out.println("Testing Selection Sort");
 		for (int i = 0; i < testCases.length; i++)
 		{
-			System.out.println("Test Case " + (i + 1) + ": Before Sorting:");
+			System.out.println("Test Case " + (i + 1) + ": Before Selection Sorting:");
 			printArray(testCases[i]); 
 			selectionSort(testCases[i]);
-			System.out.println("After Sorting:");
+			System.out.println("After selection Sorting:");
 			printArray(testCases[i]);
 			System.out.println();
-		}
+		}*/
 		
-		//buble sort
-		System.out.println("Testing Buble Sort");
+		/*//bubble sort
+		System.out.println("Testing Bubble Sort");
 		for (int i = 0; i < testCases.length; i++)
 		{
-			System.out.println("Test Case " + (i + 1) + ": Before buble Sorting:");
+			System.out.println("Test Case " + (i + 1) + ": Before bubble Sorting:");
 			printArray(testCases[i]); 
-			bubleSort(testCases[i]);
-			System.out.println("After buble Sorting:");
+			bubbleSort(testCases[i]);
+			System.out.println("After bubble Sorting:");
 			printArray(testCases[i]);
 			System.out.println();
-		}
+		}*/
+		
 		//insert sort
 		System.out.println("Testing Insert Sort");
 		for (int i = 0; i < testCases.length; i++)
@@ -59,48 +60,52 @@ public class SelectionBubleInsertSortTDD
 		for (int i = 0; i < array.length; i++  ) {
 			int currentMin = array[i];
 			int currentMinIndex = i;
+			//Find smallest element in the list
 			for ( int j = i + 1; j < array.length; j++) {
-				if (currentMin > array[j]) {
+				//compare currentMin to another element to find smaller element and its index
+				if (currentMin > array[j]) { 
 					currentMin = array[j];
-					currentMinIndex = j;
+					currentMinIndex = j; //
 				}
-			}//end for 2
+			}//end inner for loop
 			//swap if new minimum was found
 			if ( currentMinIndex != i) {
 				int temp = array[i];
 				array[i] = array[currentMinIndex];
 				array[currentMinIndex] = temp;
 			}
-		}//end for 1
+		}//end outer for loop
 	}
 
-	public static void bubleSort(int [] array)
+	public static void bubbleSort(int [] array)
 	{
+		//2 Nested for loop to repeats this process until the array is sorted
 		for (int i = 0; i < array.length -1; i++  ) {
 			for (int j = 0; j < array.length -1; j++ ) {
+				//Compare successive neighboring pairs. 
+				//If the pair is not in order, its values are swapped
 				if (array[j] > array[j+1]) {
 					int temp = array[j];
 					array[j] = array[j+1];
 					array[j+1] = temp;
 				}
-			}
-		}
+			}//end inner for loop
+		}//end outer for loop
 	
 	}//End bubleSort method
 	
 	//insertSort
 	public static void insertSort(int [] array)	{
 		for( int i = 1; i < array.length; i++) {
-			int key = array[i];
+			int key = array[i]; 
 			int j= i;
+			//compare each array[j-1] to key, if array[j-1] larger than key, assign array[j] = array[j-1] 
 			while (j > 0 && array[j-1]> key) {
 				array[j] = array[j-1];
-				j--;
+				j--; //update control variable for while loop
 			}//end while
-			array[j] = key;
-			
-			
-		}
+			array[j] = key; //update value key to array[j], j when while stop
+		}//end for loop
 	}
 	
 	public static void printArray(int[] array)
